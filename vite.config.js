4157f3cb6920d629a8ef6a86f59dec53748d5c68
choi <cme95@mentorit.co.kr>
2024-03-17 16:03:04 +0900

    저장관리소 pinia 패키지 설치 포트 변경 1995

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,21 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   // 설정 옵션들
   plugins: [vue()],
+
+  // 개발 서버 포트 설정
+  server: {
+    port: 1995,
+  },
+
+  // build 시에 모든 console.log 제거
+  build: {
+    minify: 'terser',
+    terserOptions: {
+      compress: {
+        drop_console: true,
+        drop_debugger: true
+      }
+    }
+  }
+
 })
