7099897b463c0390c9466920cbb749b8109a2b25
choi <cme95@mentorit.co.kr>
2024-03-25 23:00:32 +0900

    docker postgres 연결 수정

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from 'express';
-import { Pool } from 'pg';
+import pg from 'pg';
+const { Pool } = pg;
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -8,10 +9,10 @@ const PORT = process.env.PORT || 3000;
 const pool = new Pool({
     // docker-compose.yaml
     user: 'cmeapp', // PostgreSQL 컨테이너에 설정한 사용자 이름
-    host: 'database-server', // Docker Compose 내에서 PostgreSQL 컨테이너의 이름을 호스트로 사용
+    host: 'localhost', // Docker Compose 내에서 PostgreSQL 컨테이너의 이름을 호스트로 사용
     database: 'cmedb', // PostgreSQL 컨테이너에 설정한 데이터베이스 이름
     password: 'cmepwd', // PostgreSQL 컨테이너에 설정한 비밀번호
-    port: 9966, // Docker Compose에서 PostgreSQL 컨테이너의 포트
+    port: 5432, // Docker Compose에서 PostgreSQL 컨테이너의 포트
 });
 
 // 미들웨어 추가
@@ -19,8 +20,16 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // 라우트 정의
-app.get('/', (req, res) => {
-    res.send('Express 서버가 실행 중입니다.');
+app.get('/', async (req, res) => {
+    try {
+        const client = await pool.connect();
+        const result = await client.query('SELECT NOW()');
+        res.send('Connected to PostgreSQL: ' + result.rows[0].now);
+        client.release();
+    } catch (err) {
+        console.error('Error executing query', err);
+        res.status(500).send('Error connecting to PostgreSQL');
+    }
 });
 
 // 서버 시작
