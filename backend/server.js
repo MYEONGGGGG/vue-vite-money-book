012ff74f0d2acba4219b30273b4df2da44483687
choi <cme95@mentorit.co.kr>
2024-03-25 22:42:20 +0900

    docker로 dbms 추가, express 연결

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,18 @@
 import express from 'express';
+import { Pool } from 'pg';
+
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+// PostgreSQL 연결 정보
+const pool = new Pool({
+    // docker-compose.yaml
+    user: 'cmeapp', // PostgreSQL 컨테이너에 설정한 사용자 이름
+    host: 'database-server', // Docker Compose 내에서 PostgreSQL 컨테이너의 이름을 호스트로 사용
+    database: 'cmedb', // PostgreSQL 컨테이너에 설정한 데이터베이스 이름
+    password: 'cmepwd', // PostgreSQL 컨테이너에 설정한 비밀번호
+    port: 9966, // Docker Compose에서 PostgreSQL 컨테이너의 포트
+});
 
 // 미들웨어 추가
 app.use(express.json());
