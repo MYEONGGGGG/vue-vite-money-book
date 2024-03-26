349432bccdb0b795c2e079b0a4686d965d201cf8
choi <cme95@mentorit.co.kr>
2024-03-27 00:03:19 +0900

    db 내용 수정 1. 샘플 테이블 및 데이터 추가 2. 조회결과 HTTP 응답 보내기

--- backend/server.js
+++ backend/server.js
@@ -19,12 +19,68 @@ const pool = new Pool({
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// 테이블 생성 및 데이터 삽입
+(async () => {
+    const client = await pool.connect();
+    try {
+        // 특정 테이블 존재 여부 확인
+        const tableExistsQuery = `
+            SELECT EXISTS (
+                SELECT FROM information_schema.tables
+                WHERE table_name = 'users'
+            );
+        `;
+        const { rows } = await client.query(tableExistsQuery);
+        const tableExists = rows[0].exists;
+
+        // 테이블이 존재하지 않으면 테이블 생성 및 데이터 삽입
+        if (!tableExists) {
+            await client.query(`
+                CREATE TABLE users (
+                    id SERIAL PRIMARY KEY,
+                    username VARCHAR(50) UNIQUE NOT NULL,
+                    email VARCHAR(100) UNIQUE NOT NULL
+                )
+            `);
+
+            // 데이터 삽입
+            await client.query(`
+                INSERT INTO users (username, email)
+                VALUES ('user1', 'user1@example.com'),
+                       ('user2', 'user2@example.com')
+            `);
+
+            console.log('테이블 생성 및 데이터 삽입이 완료되었습니다.');
+        } else {
+            console.log('테이블이 이미 존재합니다.');
+        }
+    } catch (err) {
+        console.error('오류 발생:', err);
+    } finally {
+        // 클라이언트 반환
+        client.release();
+    }
+})();
+
 // 라우트 정의
 app.get('/', async (req, res) => {
     try {
+        // 데이터베이스에 연결된 클라이언트 가져오기
         const client = await pool.connect();
-        const result = await client.query('SELECT NOW()');
-        res.send('Connected to PostgreSQL: ' + result.rows[0].now);
+
+        // 생성한 테이블에서 데이터 조회
+        const result = await client.query('SELECT id, username, email FROM users');
+
+        // 조회 결과
+        let message = '[Connected to PostgreSQL] 총 건수: ' + result.rowCount;
+        for (let i=0; i<result.rowCount; i++) {
+            message = message + ' (' + (i+1) + ') id: ' + result.rows[i].id + 'username: ' + result.rows[i].username + 'email: ' + result.rows[i].email;
+        }
+
+        // Express 에서 클라이언트에게 HTTP 응답 보내기(서버 -> 클라이언트로 데이터 전송)
+        res.send(message);
+
+        // 클라이언트 반환(연결 해제)
         client.release();
     } catch (err) {
         console.error('Error executing query', err);
